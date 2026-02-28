804bf7b75ae8b8b71c17b2a46cc3098938e1541d
Px322 <prousseauu@gmail.com>
2026-02-28 23:31:45 +0100

    Deuxieme commit - chemin random

--- core/grille.py
+++ core/grille.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 class Grille:
 
@@ -10,3 +11,25 @@ class Grille:
             self.arrivee = data["arrivee"]
 
             self.grille = [[0 for _ in range(self.largeur)] for _ in range(self.hauteur)]
+
+    def creer_chemin(self):
+        x, y = self.depart
+        self.grille[x][y] = 1
+
+        while [x, y] != self.arrivee:
+            directions = []
+
+            if x < self.arrivee[0]:
+                directions.append((1, 0))
+            if x > self.arrivee[0]:
+                directions.append((-1, 0))
+            if y < self.arrivee[1]:
+                directions.append((0, 1))
+            if y > self.arrivee[1]:
+                directions.append((0, -1))
+
+            dx, dy = random.choice(directions)
+            x += dx
+            y += dy
+
+            self.grille[x][y] = 1
