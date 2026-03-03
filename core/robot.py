95e07a3ec3da8f36b46f82ed340edd1a350f9e92
Px322 <prousseauu@gmail.com>
2026-03-03 19:54:11 +0100

    Cinquième commit - Fix probleme indentation

--- core/robot.py
+++ core/robot.py
@@ -7,33 +7,33 @@ class Robot:
         self.hauteur = grille.hauteur
         self.largeur = grille.largeur
 
-def commencer(self):
-        file = [self.depart]      # la file pour BFS
-        parcouru = []             # cases déjà visitées
-        parent = {self.depart: None}  # pour reconstruire le chemin
+    def commencer(self):
+            file = [self.depart]      # la file pour BFS
+            parcouru = []             # cases déjà visitées
+            parent = {self.depart: None}  # pour reconstruire le chemin
 
-        directions = [(-1,0),(1,0),(0,-1),(0,1)]  # haut, bas, gauche, droite
+            directions = [(-1,0),(1,0),(0,-1),(0,1)]  # haut, bas, gauche, droite
 
-        while file:
-            u = file.pop(0)       
-            parcouru.append(u)
+            while file:
+                u = file.pop(0)       
+                parcouru.append(u)
 
-            if u == self.arrivee:
+                if u == self.arrivee:
                 
-                chemin = []
-                while u is not None:
-                    chemin.append(u)
-                    u = parent[u]
-                chemin.reverse()
-                return chemin
+                    chemin = []
+                    while u is not None:
+                        chemin.append(u)
+                        u = parent[u]
+                    chemin.reverse()
+                    return chemin
 
-            x, y = u
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                case = (nx, ny)
-                if 0 <= nx < self.hauteur and 0 <= ny < self.largeur:
-                    if self.grille[nx][ny] == 1 and case not in parcouru and case not in file:
-                        file.append(case)
-                        parent[case] = u
+                x, y = u
+                for dx, dy in directions:
+                    nx, ny = x + dx, y + dy
+                    case = (nx, ny)
+                    if 0 <= nx < self.hauteur and 0 <= ny < self.largeur:
+                        if self.grille[nx][ny] == 1 and case not in parcouru and case not in file:
+                            file.append(case)
+                            parent[case] = u
 
-        return None  # pas de chemin trouvé
+            return None  # pas de chemin trouvé
