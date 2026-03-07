6fe2ab8d8be977ea9120e0a3392c29c2fc1e9ea3
Px322 <prousseauu@gmail.com>
2026-03-07 22:59:52 +0100

    Suite rendu

--- ui/rendu.py
+++ ui/rendu.py
@@ -7,8 +7,17 @@ class Rendu:
         self.hauteur_pixel = grille.hauteur * self.taille_case
         self.largeur_pixel = grille.largeur * self.hauteur_pixel
 
-# pygame.init()
+        self.fenetre = pygame.display.set_mode((self.largeur_pixel, self.hauteur_pixel))
+        pygame.display.set_caption("Robot - BFS")
 
+        
+        self.BLANC = (255, 255, 255)
+        self.NOIR = (0, 0, 0)
+        self.VERT = (0, 200, 0)    # chemin
+        self.ROUGE = (200, 0, 0)   # arrivée
+        self.BLEU = (0, 0, 200)    # départ
+        self.JAUNE = (255, 220, 0) # solution BFS
 
+        def dessiner(self, chemin=None):
 
 
